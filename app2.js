1ef4c0c1ebfe3e2cdeeb3f61f5bb3346096dbc9f
Et-15 <136495322+Et-15@users.noreply.github.com>
2025-12-26 03:07:28 +0300

    app2.js

--- app2.js
+++ app2.js
@@ -1,5 +1,5 @@
 const data = require('./otherModule')
-require('./addNums') // lets see that how this is importing the details of addNUMS.js while the addNums is not exporting, is that due the node's behaviour of modules?
+require('./addNums') 
 const {items, specialPerson} = require('./otherModule')
 
 console.log(items[0])
